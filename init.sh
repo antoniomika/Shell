f1911f023e52fc4b1ca2d86de3dd144fabfdb760
Antonio Mika <antoniomika@gmail.com>
2017-02-21 10:21:12 -0500

    Initial commit

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ ! -d "~/.oh-my-zsh/" ]; then
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+fi
+
+if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+pip install --upgrade pip
+pip install --user powerline-status
+
+if [ ! -f "~/.zshrc" ]; then
+    cp .zshrc ~/.zshrc
+    POWERLINE_LOCATION=$(pip show powerline-status | grep -i "Location: " | awk '{print $2}')
+
+    sed -i "s:PIP_REPO_ROOT:$POWERLINE_LOCATION:g" ~/.zshrc
+fi
+
+mkdir -p .logs/
+mkdir -p .config/
+
+cp -R powerline/ ~/.config/powerline
+
+echo "All set! To finish install, run 'chsh -s /bin/zsh'!"
